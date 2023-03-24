fc945251ebdd5e1765c4fca972f7a569824bffe0
luyendd <doduyluyen@gmail.com>
2023-03-24 16:20:43 +0700

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Mount function to start up the app
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+// If we are in development and in isolation, call mount immediately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#dev-marketing-root');
+  if (devRoot) mount(devRoot);
+}
+// We are running through the container, we should export the mount function
+export { mount };
