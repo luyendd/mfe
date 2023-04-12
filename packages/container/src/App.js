3e7096f08d6fa1ff31813f501060937b8c03e7a1
luyendd <doduyluyen@gmail.com>
2023-04-12 16:29:27 +0700

    Add production prefix for css

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
 import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ctn',
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <StylesProvider generateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </StylesProvider>
     </BrowserRouter>
   );
 };
